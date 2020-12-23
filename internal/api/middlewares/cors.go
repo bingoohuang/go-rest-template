819b816d2448985980ccc71381b893b387bc0340
bingoohuang <bingoo.huang@gmail.com>
2020-12-23 14:25:40 +0800

    refactor code

--- internal/api/middlewares/cors.go
+++ internal/api/middlewares/cors.go
@@ -5,14 +5,15 @@ import "github.com/gin-gonic/gin"
 // CORS middleware
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		f := c.Writer.Header().Set
+		f("Access-Control-Allow-Origin", "*")
+		f("Access-Control-Allow-Credentials", "true")
+		f("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		f("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
-			return
+		} else {
+			c.Next()
 		}
-		c.Next()
 	}
 }
